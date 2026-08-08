38e7288211541261800b383b3a4e4d940133db6e
pichaautobot <brotherpicha@gmail.com>
2026-08-08 12:51:44 +0700

    test: move the PR-lifecycle tests into the workflow package

--- tests/workflow/test_pr_lifecycle.py
+++ tests/workflow/test_pr_lifecycle.py
@@ -8,7 +8,7 @@ from unittest.mock import patch
 
 from orchestrator import config, workflow
 
-from tests import workflow_pr_lifecycle_test_support as support
+from tests.workflow import pr_lifecycle_test_support as support
 
 
 class ReviewVerdictEventEmissionTest(unittest.TestCase, support._PatchedWorkflowMixin):
